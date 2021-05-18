97aaa7d017e9b965384130bae2ab12a52ea97fe2
LAPTOP-9ENHBV1Q\Sri <puneethrgowda43@gmail.com>
2021-05-18 21:16:23 +0530

    changes made

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require('express')
 const configureDB = require('./config/database')
+const path = require('path')
+
 // const router = require('./routes/api/routes')
 const app = express()
 configureDB()
 
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('Welcome to Express Server'))
 // app.use(router)
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/auth', require('./routes/api/auth'))
+
+// serve static in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 
